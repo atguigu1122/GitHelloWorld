22ec20713fd24d5e1e0a2fb9844b13809dbf6486
zs <11817471@qq.com>
2016-10-31 20:58:22 +0800

    第二次

--- src/com/atguigu/GitTest.java
+++ src/com/atguigu/GitTest.java
@@ -3,5 +3,10 @@ package com.atguigu;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("sssss");
+		int num =0;
+		for (int i = 0; i < 10; i++) {
+			num += i;
+		}
+		System.out.println(num);
 	}
 }
